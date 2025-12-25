9988b9d268828e7cfd961fd91af8bca52e58aa0a
Aarti Makwana <aartimakwana2408@gmail.com>
2025-12-25 21:36:11 +0530

    add configration in nextjs

--- histudy-nextjs-app-route/next.config.mjs
+++ histudy-nextjs-app-route/next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
+  output: "export",
     images: {
     unoptimized: true,
   },
