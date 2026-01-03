5a123d97c9f353504b3f5905c49ff53a2ea71d50
Aarti Makwana <aartimakwana2408@gmail.com>
2026-01-03 17:37:41 +0530

    implement faqs apis

--- histudy-nextjs-app-route/apiEndPoints/User/index.jsx
+++ histudy-nextjs-app-route/apiEndPoints/User/index.jsx
@@ -1,3 +1,4 @@
 // Import and export authentication-related API endpoints
 export { default as UserAuth } from "./UserAuth/index";
 export { default as UserCourses } from "./Courses/index";
+export { default as Dashboard } from "./Dashboard/index";
