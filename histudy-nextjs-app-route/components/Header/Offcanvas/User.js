9f6f3f71ee2fac3efa24989b6569115893553c0d
aarti-makwana <aartimakwana2408@.com>
2026-01-27 23:30:24 +0530

    changes some design parts

--- histudy-nextjs-app-route/components/Header/Offcanvas/User.js
+++ histudy-nextjs-app-route/components/Header/Offcanvas/User.js
@@ -46,60 +46,62 @@ const User = () => {
 
   return (
     <>
-      <div className="user-dropdown-wrapper" style={{ position: 'absolute', right: 0, top: 'calc(100% + 8px)', zIndex: 9999, minWidth: 260 }}>
-        <div className="rbt-user-menu-list-wrapper" style={{ boxShadow: '0 6px 18px rgba(0,0,0,0.08)', background: '#fff', borderRadius: 6, overflow: 'hidden' }}>
-          <div className="inner">
-            <div className="rbt-admin-profile">
-              <div className="admin-thumbnail">
-                <Image
-                  src={display.img}
-                  width={43}
-                  height={43}
-                  alt="User Images"
-                />
+      <div className="rbt-user-wrapper">
+        <div className="user-dropdown-wrapper">
+          <div className="rbt-user-menu-list-wrapper">
+            <div className="inner">
+              <div className="rbt-admin-profile">
+                <div className="admin-thumbnail">
+                  <Image
+                    src={display.img}
+                    width={43}
+                    height={43}
+                    alt="User Images"
+                  />
+                </div>
+                <div className="admin-info">
+                  <span className="name">{display.name}</span>
+                  <Link
+                    className="rbt-btn-link color-primary"
+                    href="/instructor-profile"
+                  >
+                    View Profile
+                  </Link>
+                </div>
               </div>
-              <div className="admin-info">
-                <span className="name">{display.name}</span>
-                <Link
-                  className="rbt-btn-link color-primary"
-                  href="/instructor-profile"
-                >
-                  View Profile
-                </Link>
-              </div>
+              <ul className="user-list-wrapper">
+                <li>
+                  <Link href="/instructor-dashboard">
+                    <i className="feather-briefcase"></i>
+                    <span>My Courses</span>
+                  </Link>
+                </li>
+              </ul>
+              <hr className="mt--10 mb--10" />
+              <ul className="user-list-wrapper">
+                <li>
+                  <Link href="#">
+                    <i className="feather-book-open"></i>
+                    <span>Getting Started</span>
+                  </Link>
+                </li>
+              </ul>
+              <hr className="mt--10 mb--10" />
+              <ul className="user-list-wrapper">
+                <li>
+                  <Link href="/instructor-settings">
+                    <i className="feather-settings"></i>
+                    <span>Settings</span>
+                  </Link>
+                </li>
+                <li>
+                  <a href="#" onClick={handleLogout}>
+                    <i className="feather-log-out"></i>
+                    <span>{loading ? "Logging out..." : "Logout"}</span>
+                  </a>
+                </li>
+              </ul>
             </div>
-            <ul className="user-list-wrapper">
-              <li>
-                <Link href="/instructor-dashboard">
-                  <i className="feather-briefcase"></i>
-                  <span>My Courses</span>
-                </Link>
-              </li>
-            </ul>
-            <hr className="mt--10 mb--10" />
-            <ul className="user-list-wrapper">
-              <li>
-                <Link href="#">
-                  <i className="feather-book-open"></i>
-                  <span>Getting Started</span>
-                </Link>
-              </li>
-            </ul>
-            <hr className="mt--10 mb--10" />
-            <ul className="user-list-wrapper">
-              <li>
-                <Link href="/instructor-settings">
-                  <i className="feather-settings"></i>
-                  <span>Settings</span>
-                </Link>
-              </li>
-              <li>
-                <a href="#" onClick={handleLogout}>
-                  <i className="feather-log-out"></i>
-                  <span>{loading ? "Logging out..." : "Logout"}</span>
-                </a>
-              </li>
-            </ul>
           </div>
         </div>
       </div>
